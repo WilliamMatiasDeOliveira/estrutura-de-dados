655329529ecb4d9819fcb9efab98eefdabc0b5e9
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-09-25 19:57:25 -0300

    lista simples encadeada metodos inserirInicio, inserirFim, inserurPos

--- AULA_19-09/arrays/lista _exercicios/listaSimplesmenteEncadeada.js
+++ AULA_19-09/arrays/lista _exercicios/listaSimplesmenteEncadeada.js
@@ -1,24 +1,3 @@
-// class Node{
-//     constructor(valor){
-//         this.valor = valor
-//         this.proximo = null
-//     }
-// }
-
-// class ListaEncadeada{
-//     constructor(){
-//         this.inicio = null
-//         this.tamanho = 0
-//     }
-
-//     inserirInicio(valor){
-//         const novoNo = new Node(valor)
-//         novoNo.proximo = this.inicio
-//         this.inicio = novoNo
-//         this.tamanho ++
-//     }
-// }
-
 class Node{
     constructor(valor){
         this.valor = valor
@@ -32,6 +11,15 @@ class LinkedList{
         this.tamanho = 0
     }
 
+    mostrar(){
+        let atual = this.inicio
+
+        while(atual){
+            console.log(atual)
+            atual = atual.proximo
+        }
+    }
+
     inserirInicio(valor){
         const novoNo = new Node(valor)
         novoNo.proximo = this.inicio
@@ -39,17 +27,50 @@ class LinkedList{
         this.tamanho ++
     }
 
-    mostrar(){
-        let atual = this.inicio
-        while(atual){
-            console.log(atual.valor)
-            atual = atual.proximo
+    inserirFim(valor){
+        const novoNo = new Node(valor)
+
+        if(this.inicio === undefined){
+            this.inicio = novoNo
+        } else {
+            let atual = this.inicio
+
+            while(atual.proximo){
+                atual = atual.proximo
+            }
+            atual.proximo = novoNo
+        }
+        this.tamanho ++
+    }
+
+    inserirPos(valor, pos){
+        if(pos < 0 || pos > this.tamanho){
+            console.log("Posição invalida")
+            return
+        } else {
+            if(this.tamanho === 0){
+                this.inserirInicio(valor)
+                return
+            }
+           
+            const novoNo = new Node(valor)
+            let atual = this.inicio
+            let anterior
+
+            for(let i = 0; i < pos; i++){
+                anterior = atual
+                atual = atual.proximo
+            }
+
+            novoNo.proximo = atual
+            anterior.proximo = novoNo
+            this.tamanho ++
         }
     }
+
 }
 
 const lista = new LinkedList()
-
 
 lista.inserirInicio(1)
 lista.inserirInicio(2)
@@ -57,16 +78,8 @@ lista.inserirInicio(3)
 lista.inserirInicio(4)
 lista.inserirInicio(5)
 
+lista.inserirFim(30)
+
+lista.inserirPos("will", 2)
+
 lista.mostrar()
-
-
-
-
-
-
-
-
-
-
-
-
