ada3901e18fdfb4d7383e38349503839e1b7d730
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-09-26 15:15:59 -0300

    pilhas e filas

--- AULA_19-09/arrays/lista _exercicios/pilhas_filas.js
+++ AULA_19-09/arrays/lista _exercicios/pilhas_filas.js
@@ -1,12 +1,18 @@
+// ///////////////////////////////////////////////////
+// EXEMPLO PILHAS E FILAS
+// ///////////////////////////////////////////////////
+
 const estrutura = []
 let tamanho = 0
 
 function inserirInicio(valor) {
+
     for (let i = tamanho; i > 0; i--) {
         estrutura[i] = estrutura[i - 1]
     }
     estrutura[0] = valor
     tamanho++
+
 }
 
 function inserirFim(valor) {
@@ -14,58 +20,54 @@ function inserirFim(valor) {
     tamanho++
 }
 
-function deleteInicio() {
+function removerInicio() {
     if (tamanho === 0) {
-        console.log("A estrutura esta vazia")
-    } else {
-        const item = estrutura[0]
-
-        for (let i = 0; i < tamanho; i++) {
-            estrutura[i] = estrutura[i + 1]
-        }
-
-        tamanho--
-        console.log(`O item removido do inicio foi ${item}`)
+        console.log(`A estrutura esta vazia`)
     }
+    const item = estrutura[0]
+
+    for (let i = 0; i < tamanho; i++) {
+        estrutura[i] = estrutura[i + 1]
+    }
+
+    tamanho--
+    console.log(`O item removido do inicio foi ${item}`)
+
 }
 
-function deleteFim() {
+function removerFim() {
     if (tamanho === 0) {
-        console.log('A estrutura esta vazia')
-    } else {
-        const item = estrutura[tamanho - 1]
-        delete estrutura[tamanho - 1]
-        tamanho--
-        console.log(`O item removido do fim foi ${item}`)
+        console.log(`A estrutura esta vazia`)
     }
+    const item = estrutura[tamanho - 1]
+    delete estrutura[tamanho - 1]
+    tamanho--
+    console.log(`O item removido do fim foi ${item}`)
+
 }
 
-function insertPos(pos, valor) {
-    if (pos < 0 || pos > tamanho) {
-        console.log("Posição inválida")
-    } else {
-
-        for (let i = tamanho; i > pos; i--) {
-            estrutura[i] = estrutura[i - 1]
-        }
-        estrutura[pos] = valor
-        tamanho++
+function inserirPos(valor, pos) {
+    if(pos < 0 || pos > tamanho){
+        console.log(`Posição invalida`)
     }
+
+    for(let i = tamanho; i > pos; i--){
+        estrutura[i] = estrutura[i -1]
+    }
+    estrutura[pos] = valor
+    tamanho ++
 }
 
-function deletePos(pos) {
-    if (pos < 0 || pos > tamanho) {
-        console.log("Posição inválida")
-    } else {
-        const item = estrutura[pos]
+function removerPos(pos){
+    const item = estrutura[pos]
 
-        for (let i = pos; i < tamanho - 1; i++) {
-            estrutura[i] = estrutura[i + 1]
-        }
-        tamanho--
-        console.log(`O item emovido da posição ${pos} foi ${item}`)
+    for(let i = pos; i < tamanho -1; i++){
+        estrutura[i] = estrutura[i +1]
     }
+    tamanho --
+    console.log(`O item removido da posição ${pos} foi ${item}`)
 }
+
 
 
 
@@ -78,15 +80,17 @@ inserirInicio(3)
 inserirInicio(4)
 inserirInicio(5)
 
-inserirFim(30)
+// removerPos(2)
 
-deleteInicio()
+// inserirPos("will", 2)
 
-insertPos(2, "will")
+// inserirFim(30)
 
-deleteFim()
+// removerFim()
 
-deletePos(3)
+// removerInicio()
+
+
 
 
 
